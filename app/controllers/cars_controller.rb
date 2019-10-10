75821496c099efd155234a9ed4de9fbb9ec62327
Gerard <gerard@eet.nu>
2019-10-10 09:25:59 +0200

    Get some sane automatic graph limits

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,9 +1,18 @@
 class CarsController < ApplicationController
   def index
     @cars = []
+    @maxprice = 0
+    @minprice = Car.maximum(:price)
     Car.order(:country).group_by(&:country).each do |country, cars|
-      data = cars.map { |c| [c.year.strftime('%Q').to_i, c.price ]}
+      data = cars.map do |car|
+        @minprice = car.price if car.price < @minprice
+        @maxprice = car.price if car.price > @maxprice
+        [car.year.strftime('%Q').to_i, car.price ]
+      end
       @cars << { name: country, data: data }
     end
+    
+    @minprice = @minprice.floor(-4)
+    @maxprice = @maxprice.floor(-4)
   end
 end
