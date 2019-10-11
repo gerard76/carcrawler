e52cf5603a8320effdec838232d34dad06a0ff7e
Gerard <gerard@eet.nu>
2019-10-11 13:26:53 +0200

    Added cargurus.de and broommarked.no

---
+++ lib/broommarked_no.rb
@@ -0,0 +1,25 @@
+class BroommarkedNo < Crawler
+  @start_urls = ["https://www.broommarked.no/s/search/?type=hmaAuto&sort=price-asc&page=0&b=BMW_i3"]
+  
+  def parse(response, url:, data: {})
+    json = JSON.parse(response)
+    json['data'].each do |item|
+      car = Car.new(country: 'NO')
+      
+      car.make    = "#{item['brand']} #{item['model']}"
+      car.version = item['title']
+      
+      car.url   = "https://www.broommarked.no/view/#{item['aditemid']}"
+      car.year  = Date.parse("#{item['year']}-01-01")
+      car.km    = item['km']
+      car.price = (item['price'] / 10.0132423).round
+      
+      car.save
+    end
+    
+    unless json['data'].empty?
+      current_page = Rack::Utils.parse_query(URI(url).query)['page'].to_i
+      request_to :parse, url: url.gsub("page=#{current_page}", "page=#{current_page + 1}")
+    end
+  end
+end
---
+++ lib/cargurus_de.rb
@@ -0,0 +1,26 @@
+class CargurusDe < Crawler
+  # thanks to https://github.com/FadiniGIT/CarNotifierTweeter for the entry point
+  @base_url = "https://www.cargurus.de/Cars/inventorylisting"
+  @start_urls = [
+    "#{@base_url}/ajaxFetchSubsetInventoryListing.action?entitySelectingHelper.selectedEntity=d2878"
+  ]
+  
+  def parse(response, url:, data: {})
+    json = JSON.parse(response)
+    json['listings'].each do |item|
+      car = Car.new(country: 'DE')
+      
+      base_url = CargurusDe.instance_variable_get :@base_url
+      car.url = "#{base_url}/viewDetailsFilterViewInventoryListing.action?#listing=#{item['id']}"
+      
+      car.make    = "#{item['makeName']} #{item['modelName']}"
+      car.version = item['EXT_MODEL']
+      
+      car.year  = Date.parse(item['REGISTRATION_DATE'])
+      car.km    = item['mileage']
+      car.price = item['price']
+      
+      car.save
+    end
+  end
+end
