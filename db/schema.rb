3a76d20b2657690a3a18c12b1de1da7c27100ac3
Gerard <gerard@eet.nu>
2019-10-19 09:12:58 +0200

    Added a json data column to cars for adding hashes of information I want to filter on. So far I only use it on the command line.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_16_072852) do
+ActiveRecord::Schema.define(version: 2019_10_19_052322) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 2019_10_16_072852) do
     t.boolean "visible", default: true
     t.string "currency"
     t.integer "price"
+    t.json "data", default: {}
   end
 
 end
