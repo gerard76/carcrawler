6098f4a28aa8e682962f7cb9913f124820375d22
Gerard <gerard@eet.nu>
2019-10-19 12:45:24 +0200

    Some real-world examples of my data field usage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Car.where("version ilike '%120ah%' or version ilike '%120_ah%'").update_all(data: { ah: 120 })
+Car.where("version ilike '%94ah%' or version ilike '%94_ah%'").update_all(data: { ah: 94 })
+Car.where("version ilike '%60ah%' or version ilike '%60_ah%'").update_all(data: { ah: 60 })
+
+Car.where("year < '2016-06-01'").where("data::text = '{}'").update_all(data: { ah: 60 })
+
+Car.where("version ilike '%rex%' OR version ilike '%range%'").each do |car|
+  car.update(data: car.data.merge({rex: true}))
+end
