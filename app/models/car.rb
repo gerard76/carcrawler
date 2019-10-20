gerard76/carcrawler

commit e38b01ba8b3e8eb777ef859b0f0b49b89fadb7e0

Separate make from model

app/models/car.rb
@@ -15,7 +15,8 @@ class Car < ApplicationRecord
   # Defaults:
   attribute :currency, :string, default: 'EUR'
   attribute :country,  :string, default: 'NL'
-  attribute :make,     :string, default: 'BMW i3'
+  attribute :make,     :string, default: 'BMW'
+  attribute :model,    :string, default: 'i3'
   
   # Searching in json with Ransack
   Car.pluck(Arel.sql("distinct json_object_keys(data)")).each do |key|
@@ -34,23 +35,23 @@ class Car < ApplicationRecord
     response.code == 200
   end
   
-  def price=(value)
-    self.write_attribute(:price, value.gsub(/[^0-9]/, ''))
-    
-    if version =~ /ex.*btw/i && !price.nil? && country == 'NL'
-      self.price  *= 1.21 
-    end
-  end
-  
-  def km=(value)
-    self.write_attribute(:km, value.gsub(/[^0-9]/, ''))
-    self.km *= 10 if country == 'SE'
+  def type
+    "#{make} #{model}"
   end
   
   private
   
   def cleanup
-    self.version = version.strip.sub(/^#{make}/, '').strip
+    self.version = version.strip.sub(/^#{type}/, '').strip
+    
+    self.km = km.to_s.gsub(/[^0-9]/, '')
+    self.km *= 10 if country == 'SE'
+    
+    self.price = price.to_s.gsub(/[^0-9]/, '')
+    
+    if version =~ /ex.*btw/i && !price.nil? && country == 'NL'
+      self.price  *= 1.21 
+    end
   end
   
   def set_eur

db/migrate/20191020143247_add_model_to_cars.rb
@@ -1,0 +1,5 @@
+class AddModelToCars < ActiveRecord::Migration[6.0]
+  def change
+    add_column :cars, :model, :string
+  end
+end

db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_19_052322) do
+ActiveRecord::Schema.define(version: 2019_10_20_143247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2019_10_19_052322) do
     t.string "currency"
     t.integer "price"
     t.json "data", default: {}
+    t.string "model"
   end
 
 end

lib/autotrack.rb
@@ -1,7 +1,7 @@
 
 class Autotrack < Crawler
   @start_urls = [ "https://www.autotrack.nl/tweedehands?merkIds=1a67a3d8-178b-43ee-9071-9ae7f19b316a&modelIds.1a67a3d8-178b-43ee-9071-9ae7f19b316a=e2df3c6d-6dc9-48fb-bcbe-f66a9917c471&at-switch=on&policy=accepted-20190101&&paginagrootte=90&paginanummer=1"
-   ]
+  ]
 
   def parse(response, url:, data: {})
     
@@ -10,14 +10,11 @@ class Autotrack < Crawler
       
       car.url = item.at_xpath('a')[:href]
       
-      brand = item.at_xpath("a//meta[@itemprop='brand']")[:content]
-      model = item.at_xpath("a//meta[@itemprop='model']")[:content]
-      car.make    = "#{brand} #{model}"
       car.version = item.at_xpath("a//meta[@itemprop='description']")[:content]
       
       car.price   = item.at_xpath("a//data[contains(@class, 'result-item__price')]").try(:[],:value)
       
-      car.km      = item.at_xpath("a//meta[@itemprop='mileageFromOdometer']")[:content]
+      car.km      = item.at_xpath("a//meta[@itemprop='mileageFromOdometer']").try(:[],:content)
       
       year        = item.at_xpath("a//span[@itemprop='productionDate']").text rescue nil
       car.year    = Date.parse("#{year}-01-01") rescue nil

lib/broommarked_no.rb
@@ -6,7 +6,6 @@ class BroommarkedNo < Crawler
     json['data'].each do |item|
       car = Car.new(crawler: self.class, country: 'NO', currency: 'NOK')
       
-      car.make    = "#{item['brand']} #{item['model']}"
       car.version = item['title']
       
       car.url   = "https://www.broommarked.no/view/#{item['aditemid']}"

lib/cargurus_de.rb
@@ -13,7 +13,6 @@ class CargurusDe < Crawler
       base_url = CargurusDe.instance_variable_get :@base_url
       car.url = "#{base_url}/viewDetailsFilterViewInventoryListing.action?#listing=#{item['id']}"
       
-      car.make    = "#{item['makeName']} #{item['modelName']}"
       car.version = item['EXT_MODEL']
       
       car.year  = Date.parse(item['REGISTRATION_DATE'])

commit da7f7d43498d0e4d8c88d0a3211f6be2f6bad0cc

Fix stuff I broke in e38b01ba8b3e8eb777ef859b0f0b49b89fadb7e0

app/models/car.rb
@@ -39,19 +39,25 @@ class Car < ApplicationRecord
     "#{make} #{model}"
   end
   
+  def price=(value)
+    price = value.gsub(/[^0-9]/, '')
+   
+    if version =~ /ex.*btw/i && country == 'NL'
+      price  *= 1.21 
+    end
+    self.write_attribute(:price, price)
+  end
+  
+  def km=(value)
+    km = value.gsub(/[^0-9]/, '')
+    km *= 10 if country == 'SE'
+    self.write_attribute(:km, km)
+  end
+  
   private
   
   def cleanup
     self.version = version.strip.sub(/^#{type}/, '').strip
-    
-    self.km = km.to_s.gsub(/[^0-9]/, '')
-    self.km *= 10 if country == 'SE'
-    
-    self.price = price.to_s.gsub(/[^0-9]/, '')
-    
-    if version =~ /ex.*btw/i && !price.nil? && country == 'NL'
-      self.price  *= 1.21 
-    end
   end
   
   def set_eur